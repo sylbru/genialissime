768039e960f90062b77725a833c6c1394606ef15
sylvain.brunerie <sylvain.brunerie@gmail.com>
2012-01-30 10:10:52 +0000

    Premier test

---
+++ src/com/um2/projetl3/tarotandroid/TarotAndroidActivity.java
@@ -0,0 +1,31 @@
+package com.um2.projetl3.tarotandroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class TarotAndroidActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        Button bExit = (Button) findViewById(R.id.b_exit);
+        bExit.setOnClickListener(new View.OnClickListener() {
+        	
+        	Activity parent;
+			
+			@Override
+			public void onClick(View v) {
+				parent.finish();
+				
+			}
+		});
+    }
+    
+    public void exit()
+    {
+    	
+    }
+}
