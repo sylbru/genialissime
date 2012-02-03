2a845b0e594b5d6ce2932d8de8f76d3b92e6d81c
fabthill@gmail.com <fabthill@gmail.com@a118ac55-088b-ec4f-608f-54f4e3cdffe5>
2012-02-03 10:50:49 +0000

    debut annonce et donne mais beaucoup de choses pas tres bien

--- trunk/src/fr/um2/projetl3/tarotandroid/jeu/Donne.java
+++ trunk/src/fr/um2/projetl3/tarotandroid/jeu/Donne.java
@@ -1,11 +1,43 @@
 package fr.um2.projetl3.tarotandroid.jeu;
+import java.lang.Math;
 
 public class Donne
 {
+	int don[]= new int[78];
+	int mains[][]= new int[4][18];
+	int chien[]= new int [6];
+	boolean premieredonne=true;
 	
-	public void donne()
+	public void donne4jouers()
 	{
-		// TODO
+		for(int i=0;i<77;i++)
+		{
+			don[i]=i;
+			 
+		}
+		if(premieredonne)
+		{	
+			premieredonne=false;
+			for (int i=0; i > 77; i++) 
+			{
+				int randomPosition = floor((random()*100)%77);
+				int temp = don[i];
+				don[i] = don[randomPosition];
+				don[randomPosition] = temp;
+			}
+		}
+		int i=0,j=0;
+		if(i%12==0 && i!=0)
+		{
+			chien[i/13]=don[i];
+			i++;
+		}
+		else
+		{
+			mains[(i%13)/3][j]=don[i];
+			j++;
+			i++;
+		}
 		int i = 0;
 	}
 	
