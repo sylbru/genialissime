6ab52aba0eeecaa6a715345f2062ebc66ffb86f0
jbsubils <jbsubils@a118ac55-088b-ec4f-608f-54f4e3cdffe5>
2012-02-05 14:08:09 +0000

    ecriture de la methode donne() ajout de la classe chien quelques autres modif

--- trunk/src/fr/um2/projetl3/tarotandroid/jeu/Donne.java
+++ trunk/src/fr/um2/projetl3/tarotandroid/jeu/Donne.java
@@ -8,6 +8,105 @@ public class Donne
 	int chien[]= new int [6];
 	boolean premieredonne=true;
 	
+	
+	/**
+	 * @author JB
+	 * 
+	 * ne sachant pas si le code compile je vais le mettre en commentaire (chez moi même sans rien ajouter j'ai une centaine d'erreur)
+	 * 
+	 * methode de disbrution des cartes
+	 * 
+	 * sauf erreur de ma part les calcules devrait être bon, je verifiré avec quelqu'un
+	 * 
+	 *  // ? je ne sais pas ou mettre les constantes
+	 *  
+	 *  // ! Chose à modifier :
+	 *  		j est l'indice du tableau de la donne précedente ( une fois la coupe effectué)
+	 *  		mettre les constantes au bon endroit
+	 *  		changer la valeur 6 par Chien.getNombreDeCartes() 
+	 *  		pareil pour le nombre de joueur remplacer 4 par Partie.getNombreDeJoueur()
+	 *  		à la fin il faut affecter les mains qui sont dans le tableau aux joeur
+	 *  	
+	 */
+	
+	/**
+	 public void donne()
+	 {
+		 int NOMBRE_CARTES_TOTALES=78; 
+		 int CARTES_DISTRIBU_PAR_JOUEUR = 3;
+		 
+		 int nombreDeJoeur = 4; 
+		 int numeroDuJoeur = 0; // ! j'en ai besoin pour savoir à quel joeur je vais donner les cartes
+		 
+		 int possibilitéesMisesAuChien;		 
+		 int nombreDeCartesMisesAuChien = 0;
+		 int nombreDeCartesPourLeChien = 6;
+		 
+		 int randomMin = 0;
+		 int randomMax;
+		 // random(Min/Max) permette de savoir sur quel intervalle on doit faire le random
+		 
+		 int j,l,k=0;
+		 int a,b,c; // represente les trois cartes distribué à chaque fois pour chaque joeur
+		 		 
+		 possibilitéesMisesAuChien = ( NOMBRE_CARTES_TOTALES - nombreDeCartesPourLeChien ) / CARTES_DISTRIBU_PAR_JOUEUR;
+		 
+		 
+		 while(( 6 - nombreDeCartesMisesAuChien ) == 0) 
+		 {
+			 randomMax = possibilitéesMisesAuChien - ( nombreDeCartesPourLeChien - nombreDeCartesMisesAuChien );
+			 
+			 //randomMin = randomMin + (int)(Math.random() * ((randomMax - randomMin)+1));
+			 // ! il faut que la valeur de retour soit comprise entre ]randomMin,randomMax] !
+			 
+			 // nombreDeCartesMisesAuChien = (int) Math.random()*100 % 3; 
+			 
+			 for(j=0;j<(randomMin*3);j++) // ? j'ai un doute sur le inferieur strict
+			 {
+				 if (numeroDuJoeur == nombreDeJoeur)
+				 {
+					 numeroDuJoeur = 0;
+					 a++;
+					 b++;
+					 c++;
+				 }
+				 
+				 mains[numeroDuJoeur][a] = j;
+				 mains[numeroDuJoeur][b] = ++j;
+				 mains[numeroDuJoeur][c] = ++j;
+				 // l'incrementation du j doit se faire avant l'affectation au tableau
+				 numeroDuJoeur++;
+			 }
+			 for(l=0;l<=nombreDeCartesMisesAuChien;l++)
+			 {
+				 chien[k]=j;
+				 j++;
+				 k++;
+				 randomMin++;
+			 }
+
+			 
+		}
+		 while(j<NOMBRE_CARTES_TOTALES-1)
+		 {
+			 if (numeroDuJoeur == nombreDeJoeur) 
+			 {
+				 numeroDuJoeur = 0;
+				 a++;
+				 b++;
+				 c++;
+			 }
+			 
+			 mains[numeroDuJoeur][a] = j;
+			 mains[numeroDuJoeur][b] = ++j;
+			 mains[numeroDuJoeur][c] = ++j;
+			 // l'incrementation du j doit se faire avant
+			 numeroDuJoeur++;
+		 }
+		 
+	 }
+	 /**/
+	
 	public void donne4jouers()
 	{
 		for(int i=0;i<77;i++)
@@ -18,7 +117,7 @@ public class Donne
 		if(premieredonne)
 		{	
 			premieredonne=false;
-			for (int i=0; i > 77; i++) 
+			for (int i=0; i > 77; i++) // ? il n'y a pas une invertion entre > et < ?
 			{
 				int randomPosition = (int)(Math.random()*100)%77;
 				int temp = don[i];
--- trunk/src/fr/um2/projetl3/tarotandroid/jeu/Partie.java
+++ trunk/src/fr/um2/projetl3/tarotandroid/jeu/Partie.java
@@ -4,6 +4,29 @@ import java.util.Vector;
 
 public class Partie
 {
-	private Joueur[] joueurs; //? initialisé de taille 3, 4 ou 5 selon type de partie (bien ?)
+	private Joueur[] joueurs; // ? initialisé de taille 3, 4 ou 5 selon type de partie (bien ?)
 	private Scores scores;
+	private int NombreDeJoueur; // ? initialiser la taille du tableu de joeur en fonction de cette valeur
+	
+	
+	public Joueur[] getJoueurs() {
+		return joueurs;
+	}
+	public void setJoueurs(Joueur[] joueurs) {
+		this.joueurs = joueurs;
+	}
+	public Scores getScores() {
+		return scores;
+	}
+	public void setScores(Scores scores) {
+		this.scores = scores;
+	}
+	public int getNombreDeJoueur() {
+		return NombreDeJoueur;
+	}
+	public void setNombreDeJoueur(int nombreDeJoueur) {
+		NombreDeJoueur = nombreDeJoueur;
+	}
+	
+
 }
