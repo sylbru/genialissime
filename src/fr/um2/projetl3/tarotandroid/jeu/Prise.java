a420114d6d00c3c652eb401a93c49d800a1aaeb1
sylvain.brunerie <sylvain.brunerie@gmail.com>
2012-02-06 15:26:38 +0000

    Ajout Prise.java (dans Partie, il y aura un attribut priseEnCours de type Prise, enfin je le vois comme ça pour l’instant)

---
+++ src/fr/um2/projetl3/tarotandroid/jeu/Prise.java
@@ -0,0 +1,11 @@
+package fr.um2.projetl3.tarotandroid.jeu;
+
+public class Prise
+{
+	private Joueur preneur;
+	private Contrat contrat;
+	private int points; // points réalisés (à différencier du score marqué)
+	
+	// Trucs à rajouter plus tard pour jeu à 5 (roi/joueur appelé…)
+	
+}
