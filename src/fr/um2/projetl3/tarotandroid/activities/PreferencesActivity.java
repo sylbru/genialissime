7f97969022b38539c47ffdf0e5bcbee736381865
sylvain.brunerie <sylvain.brunerie@gmail.com>
2012-02-13 00:40:19 +0000

    Premier test/exemple de préférences (utilisez le bouton Menu)

---
+++ src/fr/um2/projetl3/tarotandroid/activities/PreferencesActivity.java
@@ -0,0 +1,38 @@
+package fr.um2.projetl3.tarotandroid.activities;
+
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+
+import com.um2.projetl3.tarotandroid.R;
+
+public class PreferencesActivity extends PreferenceActivity implements
+		OnSharedPreferenceChangeListener
+{
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		
+		addPreferencesFromResource(R.xml.prefs);
+	}
+	
+	@Override
+	protected void onResume(){
+		super.onResume();
+		getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
+	}
+	
+	@Override
+	protected void onPause(){
+		super.onPause();
+		getPreferenceScreen().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(this);
+	}	
+	@Override
+	public void onSharedPreferenceChanged(SharedPreferences arg0, String arg1)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- src/fr/um2/projetl3/tarotandroid/activities/TarotAndroidActivity.java
+++ src/fr/um2/projetl3/tarotandroid/activities/TarotAndroidActivity.java
@@ -1,26 +1,75 @@
 package fr.um2.projetl3.tarotandroid.activities;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.um2.projetl3.tarotandroid.R;
 
 public class TarotAndroidActivity extends Activity
 {
 	private String data; // ? (?)
-	
+
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
+
+		Button boutonTest = (Button) findViewById(R.id.boutontest);
+		boutonTest.setOnClickListener(new View.OnClickListener()
+		{
+			@Override
+			public void onClick(View v)
+			{
+				Toast myToast = Toast.makeText(getApplicationContext(),
+						"C’est un toast.", 2);
+				myToast.show();
+				finish(); // ne ferme que cette activité, pas les autres, ni les
+							// threads ou autres trucs
+			}
+		});
+
 	}
 
-   @Override
-    protected void onPause()
-    {
-    	super.onPause();
-    	// useless atm
-    }
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu)
+	{
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.mainmenu, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item)
+	{
+		// Handle item selection
+		switch (item.getItemId())
+		{
+		case R.id.itemprefs:
+			Intent myIntent = new Intent(getApplicationContext(), PreferencesActivity.class);
+			startActivity(myIntent);
+			
+			return true;
+		case R.id.dummy:
+			Toast.makeText(getApplicationContext(), "Texte bidon", 2).show();
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
+	}
+
+	@Override
+	protected void onPause()
+	{
+		super.onPause();
+		// useless atm
+	}
 }
