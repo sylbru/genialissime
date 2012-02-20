5639b2abd2502217bef354773566c726c2ddde04
jbsubils <jbsubils@a118ac55-088b-ec4f-608f-54f4e3cdffe5>
2012-02-20 17:22:16 +0000

    phase chien et ecart faite en partie.

--- trunk/src/fr/um2/projetl3/tarotandroid/clients/Joueur.java
+++ trunk/src/fr/um2/projetl3/tarotandroid/clients/Joueur.java
@@ -6,6 +6,17 @@ import fr.um2.projetl3.tarotandroid.jeu.Main;
 
 public interface Joueur
 {
+	/* 			Il faudrait creer une méthode qui permette à un joueur de faire son ecart 
+	 * 			cette méthode devrait renvoyer l'ecart.
+	 * 			
+	 * 			il suffirait alors de faire juste un appel de cette méthode dans Partie
+	 * 
+	 * 
+	 * 			il me semble que chaque joueur (IA joueur texte et client) vont gerer cette méthode differament 
+	 * 			c'est pourquoi il me semble plus judicieux d ele mettre dans joueur
+	 */
+	
+	
 	// ! Classe incompl�te, ne pas encore utiliser cette interface!
 	// TODO Finir l'interface Joueur
 	// TODO En faire une vraie interface (au sens Java) [?]
@@ -13,6 +24,7 @@ public interface Joueur
 	public abstract int getID();
 
 	public abstract void setMain(Main pMain);
+	public abstract void addChienDansMain(Carte[] chien);
 
 	public abstract void setNomDuJoueur(String s);
 	public abstract String getNomDuJoueur();
