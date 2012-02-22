e4a443a0554998df1df4e9908f2799dc59cc01f6
sylvain.brunerie <sylvain.brunerie@gmail.com>
2012-02-22 15:19:55 +0000

    Ajout de constantes

--- src/fr/um2/projetl3/tarotandroid/jeu/Constantes.java
+++ src/fr/um2/projetl3/tarotandroid/jeu/Constantes.java
@@ -4,6 +4,8 @@ public class Constantes
 {
 	public static final int NOMBRE_CARTES_TOTALES = 78; 
 	public static final int CARTES_DISTRIBU_PAR_JOUEUR = 3;
+	public static final int TOTAL_DES_POINTS_DANS_LE_JEU = 91;
+	public static final int NOMBRE_DE_BOUTS = 3;
 	
 	// ? d'autres constantes ?
 }
