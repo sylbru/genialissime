a511df8856d25ce27fcddb701d93007b4581241d
sugardood <sugardood@gmail.com>
2012-02-25 10:20:57 +0000

    Ajout de luajava et d'une ébauche de classe JoueurIA

---
+++ src/fr/um2/projetl3/tarotandroid/clients/JoueurIA.java
@@ -0,0 +1,83 @@
+package fr.um2.projetl3.tarotandroid.clients;
+
+import fr.um2.projetl3.tarotandroid.jeu.Carte;
+import fr.um2.projetl3.tarotandroid.jeu.CarteCouleur;
+import fr.um2.projetl3.tarotandroid.jeu.Contrat;
+import fr.um2.projetl3.tarotandroid.jeu.Main;
+import org.keplerproject.luajava.*;
+
+public class JoueurIA implements Joueur {
+
+	@Override
+	public void setID(int pID) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public int getID() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void setMain(Main pMain) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void addChienDansMain(Carte[] chien) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setNomDuJoueur(String s) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public String getNomDuJoueur() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Contrat demanderAnnonce(Contrat contrat) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Carte[] demanderEcart() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Carte demanderCarte() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public CarteCouleur appelerRoi() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean possedeRoi(CarteCouleur roi) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public String nom() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
