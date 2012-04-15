f72141dd4a440f01d4df5a74fe20573b559f4dd5
sylvain.brunerie@gmail.com <sylvain.brunerie@gmail.com@a118ac55-088b-ec4f-608f-54f4e3cdffe5>
2012-04-15 16:51:51 +0000

    Réglé le problème du vecteur en décommentant ce qu’avait rajouté Kevin en commentaire. ^^ (la boucle avec fillerup dans jeuDeLaCarte, r202) Donc le vecteur fonctionne toujours comme un tableau où on met la carte du joueur i à la case numéro i. Tant que ça marche…

--- trunk/src/fr/um2/projetl3/tarotandroid/jeu/Partie.java
+++ trunk/src/fr/um2/projetl3/tarotandroid/jeu/Partie.java
@@ -476,15 +476,7 @@ public class Partie extends Thread
 	@Override
 	public void run()
 	{
-		// P.lancerPartie4JoueursTexte();
-		try
-		{
-			Thread.sleep(1000);
-		} catch (InterruptedException e)
-		{
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		// vérifier que les joueurs sont settés et tout ?
 		lancerPartie();
 	}
 
